8767c737e5b329e18e4719c5cb95e9bfbe7ffda9
Freddy-byte2004 <freddyalvarado988@gmail.com>
2025-10-12 19:13:15 -0400

    ssl

--- database/conexion.js
+++ database/conexion.js
@@ -10,7 +10,8 @@ const pool = new Pool({
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
-  ssl: { rejectUnauthorized: false } 
+  ssl: { require: true,
+    rejectUnauthorized: false } 
 });
 const conexion = pool;
 

225afeaad60569895f7c80075ed3703ebae4cefe
Freddy-byte2004 <freddyalvarado988@gmail.com>
2025-10-12 18:19:16 -0400

    archivo .env

--- database/conexion.js
+++ database/conexion.js
@@ -1,10 +1,11 @@
 const mysql=require('mysql2')
+require('dotenv').config();
 
 const conexion=mysql.createConnection({
-    host:'localhost',
-    user:'root',
-    password:'',
-    database:'diabetesIA'
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: process.env.DB_NAME
 });
 
 conexion.connect((error)=>{

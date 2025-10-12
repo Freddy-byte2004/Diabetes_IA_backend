0e1c599c410b47d81856d80b72f007277c5e29f9
Freddy-byte2004 <freddyalvarado988@gmail.com>
2025-10-12 18:46:32 -0400

    conexion a postgres

--- database/conexion.js
+++ database/conexion.js
@@ -1,18 +1,23 @@
-const mysql=require('mysql2')
-require('dotenv').config();
+const { Pool } = require('pg');
 
-const conexion=mysql.createConnection({
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASS,
-    database: process.env.DB_NAME
+const pool = new Pool({
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
 });
+const conexion = pool;
 
-conexion.connect((error)=>{
-    if(error){
-        console.error('Error de conexión: ', error);
-        return;
-    }
+conexion.on('error', (err) => {
+  console.error('Unexpected error on idle client', err);
+  process.exit(-1);
+});
+conexion.connect((err) => {
+  if (err) {
+    console.error('Error de conexión a la base de datos', err.stack);
+    return;
+  }
     console.log('Conexión exitosa a la base de datos');
 });
 

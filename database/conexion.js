d88e499b56a9b9f25471a95d7a2da9a667d48c17
Freddy-byte2004 <freddyalvarado988@gmail.com>
2025-10-12 20:32:16 -0400

    no se

--- database/conexion.js
+++ database/conexion.js
@@ -5,8 +5,12 @@ if (process.env.NODE_ENV !== 'production') {
 }
 
 const pool = new Pool({
-  connectionString: process.env.DATABASE_URL,
-  ssl: { require: true,
+    host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  ssl: { 
     rejectUnauthorized: false } 
 });
 const conexion = pool;
